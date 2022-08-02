d82f6756405f05f7076a009de3f5709f12389196
AJ-111 <100746162+AJ-111@users.noreply.github.com>
2022-08-02 12:16:56 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,53 +1,129 @@
-class MyCircularQueue:
-    def __init__(self, size: int):
-        # Write code here
+class Evaluate:
+  
+  """This class validates and evaluate postfix expression.
+  Attributes:
+      top: An integer which denotes the index of the element at the top of the stack currently.
+      size_of_stack: An integer which represents the size of stack.
+      stack: A List which acts as a Stack.
+  """
+    # Write your code here
+    
 
-    def enqueue(self, value: int) -> bool:
-        # Write code here
+  def __init__(self, size):
 
-    def dequeue(self) -> bool:
-        # Write code here
+    """Inits Evaluate with top, size_of_stack and stack.
+    Arguments:
+      size_of_stack: An integer to set the size of stack.
+    """
+    self.top = -1
+    self.size_of_stack = size
+    self.stack = [None]*size
 
-    def get_front(self) -> int:
-        # Write code here
 
-    def get_rear(self):
-        # Write code here
+  def isEmpty(self):
+    """
+    Check whether the stack is empty.
+    Returns:
+      True if it is empty, else returns False.
+    """
+    return (self.top==-1)
 
-    def is_empty(self):
-        # Write code here
 
-    def is_full(self):
-        # Write code here
+  def pop(self):
+
+    """
+    Do pop operation if the stack is not empty.
+    Returns:
+      The data which is popped out if the stack is not empty.
+    """
+    if not self.isEmpty():
+
+      k=self.stack[self.top]
+      self.top-=1
+    return k
+
+
+  def push(self, operand):
+    """
+    Push the operand to stack if the stack is not full.
+    Arguments:
+      operand: The operand to be pushed.
+    """
+    self.top+=1
+    self.stack[self.top]=operand
+
+
+  def validate_postfix_expression(self, expression):
+    """
+    Check whether the expression is a valid postfix expression.
+    Arguments:
+      expression: A String which represents the expression to be validated.
+    Returns:
+      True if the expression is valid, else returns False.
+    """
+    operatorcon=0
+    operandcon=0
+    flag=0
+    for i in expression:
+
+      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
+
+        k=int(i)
+      if(i=="-" or i=="+" or i=="*" or i=="/" or i=="^"):
+
+        operatorcon+=1
+        flag=1
+      elif(isinstance(k,int)):    
+        operandcon+=1
+        flag=1
+      else:
+        flag=0
+        break
+    if(flag==1 and operandcon==(operatorcon+1)):
+      return True
+    else:
+      return False
+
+
+  def evaluate_postfix_expression(self, expression):
+    """
+    Evaluate the postfix expression
+    Arguments:
+      expression: A String which represents the the expression to be evaluated
+    Returns:
+      The result of evaluated postfix expression.
+    """
+    flag=0
+    for i in expression:
+
+      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
+
+        self.push(i)
+      else:
+        self.push(i)
+        operator=self.pop()
+        secoperator=self.pop()
+        if(operator=="+"):
+
+          self.stack[self.top]=int(self.stack[self.top])+int(secoperator)
+        elif(operator=="-"):
+          self.stack[self.top]=int(self.stack[self.top])-int(secoperator)
+        elif(operator=="*"):
+          self.stack[self.top]=int(self.stack[self.top])*int(secoperator)
+        elif(operator=="/"):
+          flag=1
+          self.stack[self.top]=int(self.stack[self.top])/int(secoperator)
+        else:
+          self.stack[self.top]=int(self.stack[self.top])^int(secoperator)
+    
+    return int(self.stack[self.top])
 
 
 # Do not change the following code
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-data = []
-for item in input().split(','):
-    item = item.strip()
-    if item == '-':
-        data.append([])
-    else:
-        data.append([int(item)])
-obj = MyCircularQueue(data[0][0])
-result = []
-for i in range(len(operations)):
-    if i == 0:
-        result.append(None)
-    elif operations[i] == "enqueue":
-        result.append(obj.enqueue(data[i][0]))
-    elif operations[i] == "get_rear":
-        result.append(obj.get_rear())
-    elif operations[i] == "get_front":
-        result.append(obj.get_front())
-    elif operations[i] == "dequeue":
-        result.append(obj.dequeue())
-    elif operations[i] == "is_full":
-        result.append(obj.is_full())
-    elif operations[i] == "is_empty":
-        result.append(obj.is_empty())
-
-print(result)
+postfix_expression = input()  # Read postfix expression
+tokens = postfix_expression.split()
+evaluate = Evaluate(len(tokens))
+if evaluate.validate_postfix_expression(tokens):
+    print(evaluate.evaluate_postfix_expression(tokens))
+else:
+    print('Invalid postfix expression')
